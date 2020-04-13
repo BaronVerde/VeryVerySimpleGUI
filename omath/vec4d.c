e75e5130ce53a98cd6c7d1d25c17d85451f556a8
BaronVerde <BaronVerde@github.com>
2020-04-13 22:13:33 +0200

    second

--- omath/vec4d.c
+++
@@ -1,7 +0,0 @@
-
-#include "vec4d.h"
-
-inline vec4d* vec4d_set( vec4d* out, const double x, const double y, const double z, const double w ) {
-	out->x = x; out->y = y; out->z = z; out->w = w;
-	return out;
-}
